c07c564cfe27931165dc47db4af14fe147cb0b74
Julio <rodrigo_l321@hotmail.com>
2020-12-05 17:38:45 -0600

    Validacion de atributos

--- src/main/java/com/solucionexpress/microservicios/app/usuarios/controllers/AlumnoController.java
+++ src/main/java/com/solucionexpress/microservicios/app/usuarios/controllers/AlumnoController.java
@@ -6,16 +6,20 @@ import com.solucionexpress.microservicios.commons.controllers.CommonController;
  
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
 public class AlumnoController extends CommonController<Alumno, AlumnoService> {
 
-    
     @PutMapping("/{id}")
-    public ResponseEntity<?> editar(@RequestBody Alumno alumno,@PathVariable Long id){
+    public ResponseEntity<?> editar(@Valid @RequestBody Alumno alumno, BindingResult result, @PathVariable Long id){
+        if (result.hasErrors() ){
+            return this.validar(result);
+        }
         Optional<Alumno> o = service.findById(id);
         if( o.isEmpty()){
             return ResponseEntity.notFound().build();
