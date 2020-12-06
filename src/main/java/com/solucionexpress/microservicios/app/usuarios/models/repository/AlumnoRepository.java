97d5ee3971e591f98163052099fa930c351c1632
Julio <rodrigo_l321@hotmail.com>
2020-12-05 18:13:10 -0600

    paginacion

--- src/main/java/com/solucionexpress/microservicios/app/usuarios/models/repository/AlumnoRepository.java
+++ src/main/java/com/solucionexpress/microservicios/app/usuarios/models/repository/AlumnoRepository.java
@@ -5,9 +5,9 @@ import com.solucionexpress.microservicios.commons.alumnos.models.entity.Alumno;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface AlumnoRepository extends CrudRepository <Alumno,Long>{
+public interface AlumnoRepository extends PagingAndSortingRepository<Alumno,Long> {
 	
 	@Query("select a from Alumno a where a.nombre like %?1% or a.apellido like %?1%")
 	public List<Alumno>findByNombreOrApellido(String term);
